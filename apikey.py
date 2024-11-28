95d9f137e6d84578d2f2bc94124c0ed556e007d9
shamarthi sathish <125563741+sathish1523121@users.noreply.github.com>
2024-11-28 10:52:41 +0530

    Add files via upload

---
+++ apikey.py
@@ -0,0 +1,7 @@
+api_data = "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"
+
+
+
+def hello():
+    print("..........................connected..............................")
+    return "succesfull"
